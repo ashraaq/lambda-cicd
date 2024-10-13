1573b7032717c70ca25b735b0d7730f128c15643
shero km <shero@users-MacBook-Air.local>
2024-10-13 12:16:10 -0600

    V update for lambda and github actions

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
  return {
      'statusCode': 200,
-     'body': json.dumps('Hello from CICD Lambda new test!')
+     'body': json.dumps('Hello from CICD Lambda sec new test!')
     }
